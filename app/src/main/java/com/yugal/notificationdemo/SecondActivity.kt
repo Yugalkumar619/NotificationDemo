2a104a086c86dd5f089a18721124cb38f702f48c
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-16 17:44:11 +0530

    Reply received notification added

--- app/src/main/java/com/yugal/notificationdemo/SecondActivity.kt
+++ app/src/main/java/com/yugal/notificationdemo/SecondActivity.kt
@@ -1,11 +1,39 @@
 package com.yugal.notificationdemo
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        receveInput()
+    }
+
+    private  fun receveInput(){
+        val KEY_REPLY = "key_reply"
+        val intent  = this.intent
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        if(remoteInput != null){
+            val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
+            result_text_view.text = inputString
+
+            val channelId = "com.yugal.notificationdemo.channel1"
+            val notificationId = 45
+
+            val repliedNotification = NotificationCompat.Builder(this,channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentText("Your reply received" )
+                .build()
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+            notificationManager.notify(notificationId, repliedNotification)
+        }
     }
 }
