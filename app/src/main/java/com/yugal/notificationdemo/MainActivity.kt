bdd8460edf463f158c69f1603a3d90e2c958a1f9
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-16 15:26:42 +0530

    Tap Action Button

--- app/src/main/java/com/yugal/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/yugal/notificationdemo/MainActivity.kt
@@ -3,7 +3,9 @@ package com.yugal.notificationdemo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -26,12 +28,24 @@ class MainActivity : AppCompatActivity() {
     }
     private fun displayNotification(){
         val notificationId = 45
+        val tapResultIntent = Intent(this,SecondActivity::class.java).apply {
+           // flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+           // flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent:PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         val notificaton = NotificationCompat.Builder(this@MainActivity,channelId)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId, notificaton)
     }
